c1d05e508a0caa94a9eb0d18842dcc1168d9afd5
matthi <mruhland01@gmail.com>
2018-05-07 18:06:41 -0700

    added unit tests for contract

---
+++ test/Lottery.test.js
@@ -0,0 +1,82 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const { interface, bytecode } = require('../compile');
+
+// instance of contract / list of accounts that are automatically unlocked from genache
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ from: accounts[0], gas: '1000000' });
+    
+    lottery.setProvider(provider);
+});
+
+describe('Lottery Contract', () => {
+
+    it('deploys a contract', () => {
+        assert.ok(lottery.options.address);
+    });
+
+    it('should allow one player to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.5', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(players.length, 1);
+        assert.equal(players[0], accounts[0]);
+    });
+
+    it('should allow multiple players to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.5', 'ether')
+        });
+
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.5', 'ether')
+        });
+
+        await lottery.methods.enter().send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.5', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(players.length, 3);
+        assert.equal(players[0], accounts[0]);
+        assert.equal(players[1], accounts[1]);
+        assert.equal(players[2], accounts[2]);
+    });
+
+    it('requires a minimum amount of ether', async () => {
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: web3.utils.toWei('0.001', 'ether')
+            });
+            assert(false);
+        } catch(err) {
+            assert(err);
+        }
+    });
+
+});
