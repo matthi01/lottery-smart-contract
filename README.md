c29ac8ff8541ac2b68666a63bf913c08f0d5af01
matthi <mruhland01@gmail.com>
2018-05-07 17:04:54 -0700

    set up the contract / compile / and deploy files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<p>Lottery Smart Contract</p>
+<p><b>Lottery Smart Contract</b></p>
 
 <p>This contract consists of a prize pool (collection of ether) and players.</p>
 <p>Once a player has submitted money to the contract (prize pool) they will automatically be recorded as a player in the lottery. </p>
@@ -20,3 +20,11 @@
     <li>enter</li>
     <li>pickWinner</li>
 </ul>
+
+<br />
+<br />
+
+<p>You don't have access to a random number function with solidity... <br />
+instead using the Current Block Difficulty, the Current Time, and Addresses Of Players, hashing them, and then using the 
+hex number to pick a player. <br />
+hex % players.length --> random number between 0 and players.length </p>
---
+++ deploy.js
@@ -0,0 +1,33 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+// wow!! - bytecode needs to have the 0x hex prefix to deploy
+const bytecodeData = '0x' + bytecode;
+
+// need to unlock an account to use for the source of ether for the deployment of the contract
+// arguments - account mnemonic + which node to connect to in the network
+const provider = new HDWalletProvider(
+    '12 word mnemonic...',
+    'https://rinkeby.infura.io/KsAC9kkwIlMDfaC6YzqT'
+);
+
+const web3 = new Web3(provider);
+
+//get accounts from web3 instance
+//new contract - remember the interface (ABI) is in JSON
+//deploy - bytecode (data) and arguments
+//send - account
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('using account: ' + accounts[0]);
+
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecodeData })
+        .send({ from: accounts[0], gas: '1000000' });
+    
+    console.log('Contract Address: ', result.options.address);
+};
+deploy();
